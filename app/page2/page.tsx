fbc38f75bdc4414ee3a07e6fb7d170dd760f92dc
Srrw <h2toan@gmail.com>
2023-07-05 00:01:44 +0700

    add page 2

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -6,7 +6,7 @@ function App() {
       {/*When I click at the Link in bottom, it only scrolls to the top component of this page*/}
       <div style={{ height: "2000px", backgroundColor: "gray" }}></div>
       <div style={{ height: "20px", backgroundColor: "green" }}>
-        You are at page 1<Link href="/page2">To Page 2</Link>
+        You are at page 2 <Link href="/">To Page 1</Link>
       </div>
     </>
   );
